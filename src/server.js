e0ebd1fc441d0212b3a42faad54026efbb47887d
Abhishek Kumar <staravii143@gmail.com>
2026-03-28 22:10:24 +0530

    Simplify database connection handling
    Removed middleware for database connection check and established connection directly.

--- src/server.js
+++ src/server.js
@@ -2,20 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
-import connectDB, { isConnected } from "./config/db.js";
+import connectDB from "./config/db.js";
 
-app.use(async (req, res, next) => {
-  try {
-    if (!isConnected) {
-      await connectDB();
-    }
-    next();
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Database connection failed"
-    });
-  }
-});
+await connectDB();
 
 export default app;
